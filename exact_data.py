bbf94275de8178ec395bb5dd35dbf0308af6e146
sggfang <sggfang@gmail.com>
2021-12-08 13:13:24 -0500

    add average and max results

--- exact_data.py
+++ exact_data.py
@@ -36,14 +36,35 @@ def main(data_path):
 										elif "icache.overall_miss_rate" in line:
 												tifs_rate[bench] = float(line.split()[1])
 		wins = []
+		ipc_ave = 0
+		ipc_max = 0
+		lat_ave = 0
+		lat_max = 0
+		rate_ave = 0
+		rate_max = 0
 		for k in baseline.keys():
 				if baseline[k] < tifs[k]:
 						wins.append(k)
 		for k in wins:
-				# print(k, str(100*(tifs[k]-baseline[k])/baseline[k])+"%")
-				written_lines.append(k+": (ipc_diff, "+str(100*(tifs[k]-baseline[k])/baseline[k])
-															+"%); (miss_latency_diff, "+str(100*(tifs_lat[k]-baseline_lat[k])/baseline_lat[k])
-															+"%); (miss_rate, "+str(baseline_rate[k])+", "+str(tifs_rate[k])+")\n")
+				# print(k, str(100*(tifs[k]-baseline[k])/baseline[k])+"%"
+				ipc_diff = 100*(tifs[k]-baseline[k])/baseline[k]
+				miss_lat_diff = 100*(tifs_lat[k]-baseline_lat[k])/baseline_lat[k]
+				miss_rate_diff = 100*(tifs_rate[k]-baseline_rate[k])/baseline_rate[k]
+				if ipc_max < ipc_diff:
+						ipc_max = ipc_diff
+				ipc_ave += ipc_diff
+				if lat_max > miss_lat_diff:
+						lat_max = miss_lat_diff
+				lat_ave += miss_lat_diff
+				if rate_max > miss_rate_diff:
+						rate_max = miss_rate_diff
+				rate_ave += miss_rate_diff
+				written_lines.append(k+": (ipc_diff, "+str(ipc_diff)
+															+"%); (miss_latency_diff, "+str(miss_lat_diff)
+															+"%); (miss_rate, "+str(miss_rate_diff)+"%\n")
+		written_lines.append("ipc_max: "+str(ipc_max)+"; ipc_ave: "+str(ipc_ave/len(wins))+"\n")
+		written_lines.append("lat_max: "+str(lat_max)+"; lat_ave: "+str(lat_ave/len(wins))+"\n")
+		written_lines.append("rate_max: "+str(rate_max)+"; rate_ave: "+str(rate_ave/len(wins))+"\n")
 
 
 if __name__ == '__main__':
