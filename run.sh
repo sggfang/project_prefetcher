4cee77be99ba21b63598155efbab37ffe5d59bb3
sggfang <sggfang@gmail.com>
2021-12-08 15:28:45 -0500

    update ru.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# ./build/ARM/gem5.opt -d out_tifs_"$1" configs/spec2k6/run.py -W 10000000 --maxinsts=10000000 --cpu-type=DerivO3CPU --l1d_size=64kB --l1i_size="$2"kB --caches --l2cache -b $1 --l1i-hwp-type TIFSPrefetcher
-
-# ./build/ARM/gem5.opt -d out_baseline_"$1" configs/spec2k6/run.py -W 10000000 --maxinsts=10000000 --cpu-type=DerivO3CPU --l1d_size=64kB --l1i_size="$2"kB --caches --l2cache -b $1 
-
 # bzip2 gcc bwaves mcf milc
 benchmark=(bzip2 gcc bwaves mcf milc zeusmp gromacs leslie3d namd gobmk dealII povray calculix hmmer sjeng GemsFDTD libquantum h264ref tonto lbm omnetpp astar xalancbmk specrand_i specrand_f)
 # benchmark=(leslie3d gromacs GemsFDTD xalancbmk mcf sjeng gcc hmmer calculix astar milc povray tonto gobmk)
@@ -17,13 +13,11 @@ do
 
 				# ./build/ARM/gem5.opt -d data_jump_"$s"/out_tifs_"$ele" configs/spec2k6/run.py -r 100000 -W 1000000 -I 1000000 --checkpoint-dir=jump_checkpoint/"$ele" --cpu-type=TimingSimpleCPU --restore-with-cpu=DerivO3CPU --l1d_size=64kB --l1i_size="$s"kB --caches --l2cache --l2_size=8192kB -b $ele --l1i-hwp-type TIFSPrefetcher
 
-				./build/ARM/gem5.opt -d data_mem_"$s"/out_tifs_"$ele" configs/spec2k6/run.py -W 10000000 --maxinsts=10000000 --cpu-type=DerivO3CPU --l1d_size=64kB --l1i_size="$s"kB --caches --l2cache --l2_size=8192kB -b $ele --l1i-hwp-type TIFSPrefetcher
+				./build/ARM/gem5.opt -d data_mem_"$s"/out_tifs_"$ele" configs/spec2k6/run.py -W 50000000 --maxinsts=1000000000 --cpu-type=DerivO3CPU --l1d_size=64kB --l1i_size="$s"kB --caches --l2cache --l2_size=8192kB -b $ele --l1i-hwp-type TIFSPrefetcher
 
-			#	./build/ARM/gem5.opt -d data_temp_"$s"/out_tifs_"$ele" configs/spec2k6/run.py -W 10000000 --maxinsts=1000000 --cpu-type=DerivO3CPU --l1d_size=64kB --l1i_size="$s"kB --caches --l2cache -b $ele --l1i-hwp-type TIFSPrefetcher
 
-			./build/ARM/gem5.opt -d data_mem_"$s"/out_stride_"$ele" configs/spec2k6/run.py -W 10000000 --maxinsts=10000000 --cpu-type=DerivO3CPU --l1d_size=64kB --l1i_size="$s"kB --caches --l2cache --l2_size=8192kB -b $ele --l1i-hwp-type StridePrefetcher
+			./build/ARM/gem5.opt -d data_mem_"$s"/out_stride_"$ele" configs/spec2k6/run.py -W 50000000 --maxinsts=1000000000 --cpu-type=DerivO3CPU --l1d_size=64kB --l1i_size="$s"kB --caches --l2cache --l2_size=8192kB -b $ele --l1i-hwp-type StridePrefetcher
 
-			#	./build/ARM/gem5.opt -d data_temp_"$s"/out_baseline_"$ele" configs/spec2k6/run.py -W 10000000 --maxinsts=1000000 --cpu-type=DerivO3CPU --l1d_size=64kB --l1i_size="$s"kB --caches --l2cache -b $ele
 		done
 done
 
